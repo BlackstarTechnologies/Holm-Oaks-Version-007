786270edead28af29b771b9ee79db4d3b8ae58e1
iRobot98 <matendejudah@gmail.com>
2023-04-28 09:12:18 +0300

    vscode issues

--- src/utils/render_gallery.js
+++ src/utils/render_gallery.js
@@ -1,2 +1,8 @@
 const fs = require("fs")
-const $ = require("cheerio")
+const cheerio = require("cheerio")
+const BlogPath = "./views/Blog/index.html"
+const $ = cheerio.load(fs.readFileSync(BlogPath).toString())
+
+console.log($?"loaded":"not loaded")
+
+$("")
