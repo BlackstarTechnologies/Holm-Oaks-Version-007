3ed59fb1ece9161699bca3cdea3517e1dde5fb12
iRobot98 <matendejudah@gmail.com>
2023-05-04 12:08:58 +0300

    working on gallery

--- views/Gallery/Gallery.js
+++ views/Gallery/Gallery.js
@@ -1,1 +1,28 @@
+console.log("Hello World");
+const root = $("#root");
+const imgs_existing = root
+  .html()
+  .split('"')
+  .filter((v) => v.includes("Fotor"));
 
+// console.log(imgs_existing);
+fetch("/api/images/images-list")
+  .then((res) => res.json())
+  .then(({ images_list }) => {
+    // console.log(images_list);
+    // console.log($);
+
+    if (root) {
+      images_list
+        .filter((v) => !imgs_existing.includes(v))
+        .forEach(async (src) => {
+          var img = new Image();
+          img.src = src;
+          await fetch(src);
+          img.classList.add("gallery-image");
+
+          document.getElementById("root").appendChild(img);
+          //   appendToRoot(element);
+        });
+    }
+  });
